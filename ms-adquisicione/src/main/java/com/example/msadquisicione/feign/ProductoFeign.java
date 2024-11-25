c3bfb7831ec2723520564dd077e3d32b7960fbc8
DanielPOO <ccanadaniely@gmail.com>
2024-11-24 20:54:09 -0500

    cambios

--- ms-adquisicione/src/main/java/com/example/msadquisicione/feign/ProductoFeign.java
+++ ms-adquisicione/src/main/java/com/example/msadquisicione/feign/ProductoFeign.java
@@ -2,15 +2,17 @@ package com.example.msadquisicione.feign;
 
 
 import com.example.msadquisicione.dto.ProductoDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 
 @FeignClient(name = "ms-catalogo-service", path = "/producto")
 public interface ProductoFeign {
-    @GetMapping("/productos/{id}/existe")
+    @GetMapping("/{id}/existe")
+    @CircuitBreaker(name = "productListByIdCB", fallbackMethod = "productListById")
     Boolean verificarProducto(@PathVariable("id") Integer id);
 
-    @PutMapping("/productos/{id}/actualizar-stock")
+    @PutMapping("/{id}/actualizar-stock")
     void actualizarStock(@PathVariable("id") Integer id, @RequestParam("cantidad") Integer cantidad);
 }
--- ms-adquisicione/src/main/java/com/example/msadquisicione/feign/ProveedorFeign.java
+++ ms-adquisicione/src/main/java/com/example/msadquisicione/feign/ProveedorFeign.java
@@ -1,12 +1,14 @@
 package com.example.msadquisicione.feign;
 
 import com.example.msadquisicione.dto.ProveedorDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "ms-proveedor-service", path = "/proveedor")
 public interface ProveedorFeign {
-    @GetMapping("/proveedores/{id}")
+    @GetMapping("/{id}")
+    @CircuitBreaker(name = "proveedorListByIdCB", fallbackMethod = "proveedorListById")
     ProveedorDto obtenerProveedor(@PathVariable("id") Integer id);
 }
